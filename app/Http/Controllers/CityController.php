b794091f7eab6919899b6312db0b177e9e9896d0
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-10-09 21:57:45 +0300

    Cleared and reformatted the City controller and service classes.

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -4,18 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\City;
 use App\Services\CityService;
+use App\Services\UpdateWeatherService;
 
 class CityController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-        CityService::fetchCityData();
+        // 1.TODO: return data from json as array only name of cities -- asses to name of the method
+        $getNamesOfCitiesFromJson = CityService::getCityName(CityService::parseJsonData(CityService::getCityDataPath())->cities, []);
 
+        // 2.TODO: having the cities -- get the weather data for every of the cities ,result :array with the city and weather data -- send to the view
+        $getWeatherForACityFromApi = CityService::getCityWeather();
+        $array_with_city_and_weather_data = array_combine($getNamesOfCitiesFromJson, $getWeatherForACityFromApi);
+
+        // 3.TODO: having all data fetched, send to service to update them in the db
+        UpdateWeatherService::getCityByNameOrCreate($array_with_city_and_weather_data);
+
+        // 4.TODO: send for rendering to the view
+        // reformat
+
+        // will not need
         $all_cities = City::all();
 
         return view('cities', compact('all_cities'));
--- app/Services/CityService.php
+++ app/Services/CityService.php
@@ -2,72 +2,65 @@
 
 namespace App\Services;
 
-use App\Models\City;
-
 /**
  * Service class for holding all the loading for fetching, rendering and saving data to the database.
  */
 class CityService
 {
+
+    public static function getWeatherApi($cityName) {
+        return "https://api.openweathermap.org/data/2.5/weather?q=$cityName&units=imperial&appid=895284fb2d2c50a520ea537456963d9c";
+    }
+
+    public static function getCityDataPath() {
+        return storage_path('app/public/city_data.json');
+    }
+
     /**
-     * parse JSON Data from lacal file
+     * parse JSON Data from local file
      * @return mixed
      */
-    private static function getJsonData()
+    public static function parseJsonData($response_data)
     {
-        $path = storage_path('app/public/city_data.json');
-        $json_data = json_decode(file_get_contents($path));
+//        $path = storage_path('app/public/city_data.json');
+        $json_data = json_decode(file_get_contents($response_data));
         return $json_data;
+    }
+
+    /*
+     * Iterate the retrieved data from the object, save all city names in an array
+     * Children cities solution: Recursive function for fetching the city names and children
+     * @param $cities
+     * @return array
+     */
+    public static function getCityName($cities, array $array_holding_cities)
+    {
+        foreach ($cities as $city) {
+            $cityName = $city->name;
+            $array_holding_cities[] = $cityName;
+            if (isset($city->children)) {
+//                    $arr_with_cities = CityService::getCityName($city->children, array_values(array_holding_cities));
+                $array_holding_cities = CityService::getCityName($city->children, $array_holding_cities);
+            }
+        }
+        return $array_holding_cities;
     }
 
     /**
      * Get data for current city from API and decode it as an object
-     * @param $cityName
+     * @param $cityNames
      * @return mixed
      */
-    private static function getCityWeather($cityName)
+    public static function getCityWeather()
     {
-        $apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=$cityName&units=imperial&appid=895284fb2d2c50a520ea537456963d9c";
-        $weatherData = json_decode(file_get_contents($apiUrl));
-        return $weatherData->weather[0]->description;
-    }
-
-    /**
-     * Iterate the retrieved data from the object, save all city names in an array
-     * Children cities solution: Recursive function for fetching the city names and children
-     * @param $cities
-     * @return void
-     */
-    private static function getCityName($cities)
-    {
-        foreach ($cities as $city) {
-            $cityName = $city->name;
-            HelperGlobal::$towns[] = $cityName;
-            if (isset($city->children)) {
-                CityService::getCityName($city->children);
-            }
-        }
-    }
-
-    /**
-     *
-     * @return void
-     */
-    public static function fetchCityData()
-    {
-        // CityService::getJsonData() -> cities -- returns an object "cities"
-        // CityService::getCityName() -> iterates the object recursively and populates and outer array with the results(names of the cities)
-        CityService::getCityName(CityService::getJsonData()->cities);
-        // if data exists(old data) in db, delete ir from the db
-        if (City::where('id', '>', 0)->get('id')) {
-            City::where('id', '>', 0)->delete();
+        $array_holding_city_names = CityService::getCityName(CityService::parseJsonData(CityService::getCityDataPath())->cities, []);
+        $arr_hold_weather = [];
+        foreach($array_holding_city_names as $cityName) {
+//            $arr_hold_weather[] = self::getJsonData(self::getWeatherApi($cityName))->weather[0]->description;
+            $arr_hold_weather[] = self::parseJsonData(self::getWeatherApi($cityName));
         }
 
-        // then fill the db with the newest data from the API
-        foreach (HelperGlobal::$towns as $town) {
-            $WeatherForecast = City::create(['name' => $town, 'forecast' => CityService::getCityWeather($town)]);
-            $WeatherForecast->save();
-        }
+        return $arr_hold_weather;
     }
 
 }
