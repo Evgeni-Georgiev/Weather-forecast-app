fa8655b89d23c49315982afbab6f1902af4d259c
Evgeni Georgiev <evgenigg2000@gmail.com>
2022-10-09 23:25:54 +0300

    Moved the method for updating the data to the Model.

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
     public $timestamps = false;
     protected $fillable = [
         "id",
@@ -20,4 +21,26 @@ class City extends Model
         "pressure",
         "humidity"
     ];
+
+    public static function updateCityByNameOrCreate($array_with_city_and_weather_data)
+    {
+
+        foreach ($array_with_city_and_weather_data as $key => $value) {
+            $return_city_record = City::firstOrCreate(
+                ['name' => $key]
+            );
+
+            $return_city_record->forecast = $value->weather[0]->description;
+            $return_city_record->temperature = $value->main->temp;
+            $return_city_record->feels_like = $value->main->feels_like;
+            $return_city_record->temp_min = $value->main->temp_min;
+            $return_city_record->temp_max = $value->main->temp_max;
+            $return_city_record->pressure = $value->main->pressure;
+            $return_city_record->humidity = $value->main->humidity;
+
+            $return_city_record->save();
+
+        }
+
+    }
 }
